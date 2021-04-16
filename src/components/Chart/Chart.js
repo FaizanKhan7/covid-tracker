d812650d87d679c1229b1073aaa6f68f0e770a43
Faizan khan <mohamedfaizankhan1@gmail.com>
2021-04-16 16:20:16 +0530

    added line chart of infected and deaths due to covid

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,19 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { fetchDailyDate } from "../../api";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
+
 function Chart() {
-  const [dailyData, setDailyData] = useState({});
+  const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
     const fetchApi = async () => {
-      setDailyData(await fetchDailyDate());
+      setDailyData(await fetchDailyData());
     };
+
+    // console.log(dailyData);
     fetchApi();
-  });
-  return (
-    <div>
-      <h1>Chart</h1>
-    </div>
-  );
+  }, [dailyData]);
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0,0, 0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{lineChart}</div>;
 }
 
 export default Chart;
